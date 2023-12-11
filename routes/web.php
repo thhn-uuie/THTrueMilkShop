84fc8794f9cfb3d5a8d768e761f2a310a8a26aa1
thhn-uuie <117459480+thhn-uuie@users.noreply.github.com>
2023-12-11 16:27:53 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
+
+////Backend
+//Route::get('/admin', function () {
+//    return view('backend.index');
+//});
+//Route::prefix('category')->name('backend.category.')->group(function () {
+//    Route::get('/', [CategoryController::class, 'index'])->name('index');
+//    Route::match(['GET', 'POST'],'/create', [CategoryController::class, 'store'])->name('create');
+//    Route::match(['GET', 'POST'],'/detail/{id}', [CategoryController::class, 'show'])->name('detail');
+//    Route::get('/delete/{id}', [CategoryController::class, 'destroy'])->name('delete');
+//    Route::match(['GET', 'POST'],'/update/{id}', [CategoryController::class, 'update'])->name('update');
+//
+//});
