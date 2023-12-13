eb44a5c03b22b63d4f1b3d5e64489aa5e5f5e302
DTHUONN <98147788+Luo1604@users.noreply.github.com>
2023-12-13 23:38:34 +0700

    update jquery validation

--- resources/views/frontend/auth/fpassword.blade.php
+++ resources/views/frontend/auth/fpassword.blade.php
@@ -3,43 +3,32 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>Quên mật khẩu – TH true MILK</title>
-    <link rel="icon" href="https://www.thmilk.vn/wp-content/themes/wp-th/favicon.png" type="image/png">
+  <title>Quên mật khẩu – TH true MILK</title>
+  <link rel="icon" href="https://www.thmilk.vn/wp-content/themes/wp-th/favicon.png" type="image/png">
 
-    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
-    <link href="../../../../public/frontend/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+  <link href="../../../../public/frontend/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="../../../../public/frontend/css/style.css" rel="stylesheet">
 
-    <!-- Custom Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
-        type="text/css">
+  <!-- Custom Fonts -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"> -->
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript"
+        src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+  
 </head>
 
 <body id="page-top" class="index">
@@ -50,7 +39,7 @@
                     <p>Quên mật khẩu</p>
                 </div>
                 <div class="card-text">
-                    <form action="">
+                    <form action="" id="form-fpw">
 
                         <div class="in-group tel">
                             <label for="tel">Số điện thoại<i style="color: red;">*</i></label>
@@ -59,21 +48,19 @@
 
                         <div class="in-group password">
                             <label for="password">Mật khẩu mới<i style="color: red;">*</i></label>
-                            <input type="password" name="password" id="password" required
-                                placeholder="Nhập mật khẩu mới">
+                            <input type="password" name="password" id="password" required placeholder="Nhập mật khẩu mới">
                         </div>
 
-                        <div class="in-group email">
+                        <div class="in-group c_password">
                             <label for="password">Nhập lại mật khẩu<i style="color: red;">*</i></label>
-                            <input type="password" name="c-password" id="c-password" required
-                                placeholder="Nhập lại mật khẩu">
+                            <input type="password" name="c_password" id="c_password" required placeholder="Nhập lại mật khẩu">
                         </div>
 
-                        <!-- /.col -->
-                        <div class="in-group">
-                            <button type="submit" class="btn btn-primary btn-block">Đổi mật khẩu</button>
-                            <!-- /.col -->
-                        </div>
+							<!-- /.col -->
+							<div class="in-group">
+					  			<button type="submit" class="btn btn-primary btn-block">Đổi mật khẩu</button>
+							<!-- /.col -->
+				  		</div>
                         <!-- <div class="f-password">
                             <a href="fpassword.html">Quên mật khẩu?</a></div> -->
                     </form>
@@ -82,8 +69,48 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $.validator.addMethod(
+                "phoneFormat",
+                function (value, element) {
+                    // Kiểm tra số điện thoại chỉ chứa chữ số và có độ dài 9
+                    return this.optional(element) || /^\d{9}$/.test(value);
+                },
+                "Số điện thoại không hợp lệ."
+            );
+            $("#form-fpw").validate({
+                rules: {
+                    c_password: {
+                        required: true,
+                    },
+                    password: {
+                        required: true,
+                    },
+                    tel: {
+                        required: true,
+                        phoneFormat: true,
+                    }
+                },
+                messages: {
+                    c_password: {
+                        required: "Vui lòng nhập lại mật khẩu mới"
+                    },
+                    password: {
+                        required: "Vui lòng nhập mật khẩu mới"
+                    },
+                    tel: {
+                        required: "Vui lòng nhập số điện thoại",
+                        digits: "Số điện thoại chỉ được chứa chữ số"
+                    }
+                }
+            });
+        });
+    </script>
+
+
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
