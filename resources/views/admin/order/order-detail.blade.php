a123e6eeef43b12be7a0569b848ed5a6b89a5e9c
thhn-uuie <117459480+thhn-uuie@users.noreply.github.com>
2023-12-18 20:58:59 +0700

    up

--- resources/views/admin/order/order-detail.blade.php
+++ resources/views/admin/order/order-detail.blade.php
@@ -2,18 +2,7 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Administrative – TH true MILK</title>
-	<link rel="icon" href="https://www.thmilk.vn/wp-content/themes/wp-th/favicon.png" type="image/png">
-	<!-- Google Font: Source Sans Pro -->
-	<link rel="stylesheet"
-		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="../../../public/admin/plugins/fontawesome-free/css/all.min.css">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="../../../public/admin/css/adminlte.min.css">
-	<link rel="stylesheet" href="../../../public/admin/css/custom.css">
+	@include('admin.component.head')
 </head>
 
 <body class="sidebar-mini" style="height: auto;">
@@ -264,15 +253,7 @@
 		<!-- /.content-wrapper -->
 
 	</div>
-	<!-- ./wrapper -->
-	<!-- jQuery -->
-	<script src="../../../public/admin/plugins/jquery/jquery.min.js"></script>
-	<!-- Bootstrap 4 -->
-	<script src="../../../public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<!-- AdminLTE App -->
-	<script src="../../../public/admin/js/adminlte.min.js"></script>
-	<!-- AdminLTE for demo purposes -->
-	<script src="../../../public/admin/js/demo.js"></script>
+	@include('admin.component.script')
 </body>
 
 </html>
