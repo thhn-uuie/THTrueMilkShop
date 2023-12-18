0e7ac9d13fd3de7bc6bb5a742aeebcfd88a3dc7c
DTHUONN <98147788+Luo1604@users.noreply.github.com>
2023-12-18 22:47:57 +0700

    update app.js

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -1,4 +1,3 @@
-// Function to fetch and parse JSON data from a file
 async function fetchJsonData(url) {
     try {
         const response = await fetch(url);
@@ -17,69 +16,62 @@ async function fetchJsonData(url) {
 const jsonFilePath = 'public/frontend/json/products.json';
 let jsonData;
 
-// Fetch and parse JSON data
 fetchJsonData(jsonFilePath)
     .then((data) => {
         jsonData = data;
 
         function createProductElement(product) {
             const cardDiv = document.createElement('div');
-            cardDiv.className = 'c-listitem3__card1 item1-js';
-            cardDiv.setAttribute('data-product-id', product.id);
-
+            cardDiv.className = 'c-listitem3__card1';
+        
             const imgDiv = document.createElement('div');
             imgDiv.className = 'c-listitem3__img1';
-
+        
             const img = document.createElement('img');
             img.src = product.image;
             img.alt = product['image-alt'];
-
+        
             imgDiv.appendChild(img);
-
+        
             const contentArticle = document.createElement('article');
             contentArticle.className = 'c-listitem3__content1';
-
+        
             const titleH4 = document.createElement('h4');
             titleH4.className = 'title1';
             titleH4.textContent = product.name;
-
+        
             const infoDiv = document.createElement('div');
             infoDiv.className = 'info1';
-
+        
             const priceSpan = document.createElement('span');
             priceSpan.className = 'info1__price1';
             priceSpan.textContent = product.price + ' ₫';
-
-            // Create a new div for the "add" text and cart icon
+        
             const addItemDiv = document.createElement('div');
-            addItemDiv.className = 'c-add-item addCart';
-
+            addItemDiv.className = 'c-add-item';
+            addItemDiv.setAttribute('onclick', `addToCart(${product.id})`);
+        
             const addText = document.createTextNode('Thêm ');
-
+        
             const cartIcon = document.createElement('i');
             cartIcon.className = 'fa fa-solid fa-cart-shopping fa-lg';
-
-            // Append the "Add" text and cart icon to the new div
+        
             addItemDiv.appendChild(addText);
             addItemDiv.appendChild(cartIcon);
-
-            // Append the price and the new div to the infoDiv
+        
             infoDiv.appendChild(priceSpan);
             infoDiv.appendChild(addItemDiv);
-
-            // Append the title and infoDiv to the contentArticle
+        
             contentArticle.appendChild(titleH4);
             contentArticle.appendChild(infoDiv);
-
-            // Append the imgDiv and contentArticle to the cardDiv
+        
             cardDiv.appendChild(imgDiv);
             cardDiv.appendChild(contentArticle);
-
+        
             return cardDiv;
         }
+        
 
-
-        // Function to add products to the HTML
         function addProductsToHTML(products, containerId) {
             const container = document.getElementById(containerId);
 
@@ -90,11 +82,11 @@ fetchJsonData(jsonFilePath)
                         const currentRoot = document.createElement('div');
                         currentRoot.className = 'c-listitem3-root1';
                         container.appendChild(currentRoot);
-
+        
                         const listItemDiv = document.createElement('div');
                         listItemDiv.className = 'c-listitem3';
                         currentRoot.appendChild(listItemDiv);
-
+        
                         const productElement = createProductElement(product);
                         listItemDiv.appendChild(productElement);
                     } else {
@@ -110,26 +102,39 @@ fetchJsonData(jsonFilePath)
         addProductsToHTML(data.filter(product => product.category === 'suatuoitiettrung'), 'suatuoitiettrung');
     });
 
-// Keep track of the shopping cart
-const shoppingCart = [];
+let shoppingCart = [];
 
-// Function to update the shopping cart
+function saveCartToStorage() {
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+}
+
+function loadCartFromStorage() {
+    const storedCart = localStorage.getItem('shoppingCart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+        updateCartIcon();
+        updateCartDropdown();
+    }
+}
+
+window.addEventListener('load', loadCartFromStorage);
+
 function updateCart(product) {
     const existingProductIndex = shoppingCart.findIndex(item => item.id === product.id);
 
     if (existingProductIndex !== -1) {
-        // Product already exists in the cart, update quantity
         shoppingCart[existingProductIndex].quantity++;
     } else {
-        // Product is not in the cart, add it with quantity 1
         shoppingCart.push({ ...product, quantity: 1 });
     }
 
-    // Update the cart icon with the total quantity
     updateCartIcon();
+
+    updateCartDropdown();
+
+    saveCartToStorage();
 }
 
-// Function to update the cart icon with the total quantity
 function updateCartIcon() {
     const cartIcon = document.querySelector('.icon-cart span');
     if (cartIcon) {
@@ -138,19 +143,107 @@ function updateCartIcon() {
     }
 }
 
-// Event listener for cart icons
-document.addEventListener('click', function (event) {
-    const target = event.target;
+function updateCartDropdown() {
+    const productInfo = document.querySelector('.product-info');
+    const totalPriceSpan = document.getElementById('totalPrice');
 
-    if (target.classList.contains('addCart')) {
-        // Find the corresponding product in the JSON data
-        const productId = parseInt(target.closest('.c-listitem3__card1').getAttribute('data-product-id'));
-        const product = jsonData.find(item => item.id === productId);
+    productInfo.innerHTML = '';
 
-        if (product) {
-            // Update the cart and refresh the cart icon
-            updateCart(product);
-            updateCartIcon();
-        }
+    if (shoppingCart.length === 0) {
+        const emptyMessage = document.createElement('div');
+        emptyMessage.textContent = '';
+        productInfo.appendChild(emptyMessage);
+    } else {
+        shoppingCart.forEach(item => {
+            const cartItem = document.createElement('div');
+            cartItem.classList.add('cart-item');
+
+            const img = document.createElement('img');
+            img.src = item.image;
+            img.alt = item['image-alt'];
+            cartItem.appendChild(img);
+
+            const closeIconContainer = document.createElement('div');
+            closeIconContainer.classList.add('close-icon');
+            closeIconContainer.onclick = () => removeItemFromCart(item.id);
+
+            const closeIcon = document.createElement('i');
+            closeIcon.classList.add('fas', 'fa-times-circle');
+
+            closeIconContainer.appendChild(closeIcon);
+            cartItem.appendChild(closeIconContainer);
+
+            const info = document.createElement('div');
+            info.classList.add('info');
+
+            const productName = document.createElement('div');
+            productName.classList.add('product-name');
+            productName.textContent = item.name;
+            info.appendChild(productName);
+
+            const productPrice = document.createElement('div');
+            productPrice.classList.add('product-price');
+            productPrice.textContent = `${item.price} ₫`;
+            info.appendChild(productPrice);
+
+            const productQuantity = document.createElement('div');
+            productQuantity.classList.add('product-quantity');
+            productQuantity.textContent = `Số lượng: ${item.quantity}`;
+            info.appendChild(productQuantity);
+
+            cartItem.appendChild(info);
+            productInfo.appendChild(cartItem);
+        });
     }
-});
+
+    totalPriceSpan.textContent = `${calculateTotalPrice()} ₫`;
+}
+
+function removeItemFromCart(itemId) {
+    const itemIndex = shoppingCart.findIndex(item => item.id === itemId);
+    if (itemIndex !== -1) {
+        shoppingCart.splice(itemIndex, 1);
+        updateCartDropdown();
+    }
+
+    const cartIcon = document.querySelector('.icon-cart span');
+    if (cartIcon) {
+        const totalQuantity = shoppingCart.reduce((total, item) => total + item.quantity, 0);
+        cartIcon.textContent = totalQuantity;
+
+        updateCartDropdown();
+    }
+
+    saveCartToStorage();
+}
+
+function calculateTotalPrice() {
+    return shoppingCart.reduce((total, item) => total + item.quantity * item.price, 0);
+}
+
+function addToCart(productId) {
+    const product = jsonData.find(item => item.id === productId);
+    if (product) {
+        // Update the cart and refresh the cart icon and dropdown
+        updateCart(product);
+    }
+}
+
+function toggleCartDropdown() {
+    const cartDropdown = document.querySelector('.cart-dropdown');
+    if (cartDropdown) {
+        cartDropdown.classList.toggle('show');
+        console.log('show')
+    }
+}
+
+document.onclick = function (event) {
+    const target = event.target;
+    const cartDropdown = document.querySelector('.cart-dropdown');
+
+    const isIconCartClick = target.classList.contains('icon-cart') || target.classList.contains('close-icon') || target.closest('.close-icon') || target.closest('.icon-cart');
+
+    if (cartDropdown && !cartDropdown.contains(target) && !isIconCartClick) {
+        cartDropdown.classList.remove('show');
+    }
+};
