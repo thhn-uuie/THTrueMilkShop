4a1bb8beea8f63c583c2af887b2fb2a3f94351ea
DTHUONN <98147788+Luo1604@users.noreply.github.com>
2023-12-20 01:21:28 +0700

    update thanh toán

--- resources/views/frontend/user/checkout.blade.php
+++ resources/views/frontend/user/checkout.blade.php
@@ -70,28 +70,38 @@
 
                     <!-- Shopping cart tạm thế đã -->
                     <div class="navbar-nav navbar-right">
-                        <div class="icon-cart" onclick="toggleCartDropdown()">
-                            <i class="fa fa-solid fa-cart-shopping fa-lg"></i>
-                            <span>0</span>
-                        </div>
-
-                        <div class="cart-dropdown">
-                            <div class="header">
-                                Thông tin giỏ hàng
-                            </div>
-                            <div class="product-info">
-                                <!-- Dynamic content will be added here -->
-                            </div>
-                            <div class="total-price">
-                                <span class="label">Tổng tiền: </span>
-                                <span id="totalPrice">0 ₫</span>
-                            </div>
-                            <div class="actions">
-                                <div class="action viewcart">Xem giỏ hàng</div>
-                                <div class="action checkout">Thanh toán</div>
+                        <div class="login">
+                            <div class="icon-login">
+                                <i class="fa fa-solid fa-user fa-lg"></i>
+                                <a href="../auth/login.html">
+                                    <span class="lg">Đăng nhập</span>
+                                    <span>Tài khoản</span>
+                                </a>
                             </div>
                         </div>
+                        <div class="cart">
+                            <div class="icon-cart" onclick="toggleCartDropdown()">
+                                <i class="fa fa-solid fa-cart-shopping fa-lg"></i>
+                                <span>0</span>
+                            </div>
 
+                            <div class="cart-dropdown">
+                                <div class="header">
+                                    Thông tin giỏ hàng
+                                </div>
+                                <div class="product-info">
+                                    <!-- Dynamic content will be added here -->
+                                </div>
+                                <div class="total-price">
+                                    <span class="label">Tổng tiền: </span>
+                                    <span id="totalPrice">0 ₫</span>
+                                </div>
+                                <div class="actions">
+                                    <div class="action viewcart">Xem giỏ hàng</div>
+                                    <div class="action checkout">Thanh toán</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -103,33 +113,26 @@
                             <li class="page-scroll">
                                 <a href="shop.html">CÂU CHUYỆN THẬT TH</a>
                             </li>
-                            <li class="page-scroll has-children">
+                            <li class="page-scroll">
                                 <a href="#">SẢN PHẨM</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Công bố sản phẩm</a></li>
-                            </li>
-                        </ul>
-                        </li>
 
-                        <li class="page-scroll">
-                            <a href="#">Khuyến mãi</a>
-                        </li>
-                        <li class="page-scroll">
-                            <a href="#">Cửa hàng</a>
-                        </li>
-                        <li class="page-scroll has-children">
-                            <a href="#">Truyền thông</a>
-                            <ul class="dropdown">
-                                <li><a href="#">Thông tin tư vấn sử dụng sữa</a></li>
-                                <li><a href="#">Bí quyết khỏe đẹp tự nhiên</a></li>
-                                <li><a href="#">Bí quyết chăm sóc con cái</a></li>
-                                <li><a href="#">Phong cách sống xanh</a></li>
-                        </li>
-                        </ul>
-                        </li>
-                        <li class="page-scroll">
-                            <a href="#">Tuyển dụng</a>
-                        </li>
+                            </li>
+                            </li>
+
+                            <li class="page-scroll">
+                                <a href="#">Khuyến mãi</a>
+                            </li>
+                            <li class="page-scroll">
+                                <a href="#">Cửa hàng</a>
+                            </li>
+                            <li class="page-scroll">
+                                <a href="#">Truyền thông</a>
+
+                            </li>
+                            </li>
+                            <li class="page-scroll">
+                                <a href="#">Tuyển dụng</a>
+                            </li>
                         </ul>
                     </div>
                     <!-- /.navbar-collapse -->
@@ -141,12 +144,162 @@
         <div class="context">
             <div class="container">
                 <span class="item"><a href="#">Người dùng > </a></span>
-        <span class="item">Tài khoản > Thanh toán</span>
+                <span class="item">Tài khoản > Thanh toán</span>
             </div>
         </div>
 
         <!-- làm tiếp ở đây nẻ -->
-        
+        <div class=" account shipping">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12"></div>
+
+                    <div class="col-lg-8 col-md-8 order-1">
+                        <div class="column main">
+                            <form action="" id="checkout">
+                                <div class="block-title">
+                                    ĐỊA CHỈ GIAO HÀNG
+                                    <div class="item" >
+                                      <div class="address">
+                                          <div class="name">
+                                            
+                                          </div>
+                                          <div class="phone">
+                                              
+                                          </div>
+                                          <div class="addr">
+                                             
+                                          </div>
+                                          <div class="btns">
+                                              <a href="#address" data-toggle="collapse"> <i class="fa fa-lg fa-edit"></i></a>
+                                          </div>
+                              
+                                      </div>
+                                  </div>
+                                </div>
+      
+                                <a class="btn btn-primary" href="#address" data-toggle="collapse">Tạo địa chỉ</a>
+      
+                                <div class="form-address collapse" id="address">
+                                  <!-- <form action=""> -->
+                                      <div class="row">
+                                          <div class="col-xs-6">
+                                              <label for="name">Họ và tên <i style="color: red;">*</i></label>
+                                              <input type="text" name="name" id="name" required>
+                                          </div>
+      
+                                          <div class="col-xs-6">
+                                              <label for="tel">Số điện thoại <i style="color: red;">*</i></label>
+                                              <input type="tel" name="tel" id="tel" required>
+                                          </div>
+      
+                                          <div class="col-xs-6">
+                                              <label for="addr">Địa chỉ <i style="color: red;">*</i></label>
+                                              <input type="text" name="addr" id="addr" required>
+                                          </div>
+      
+                                          <!-- <div class="col-xs-12">
+                                              <button type="submit">Lưu địa chỉ</button>
+                                          </div> -->
+                                      </div>
+                                  <!-- </form> -->
+                                </div>
+                                  <!-- <button class="action checkout" onclick="clearUserOrders()">Clear</button> -->
+                                  <div class="block-content ship-content">
+                                      <span>Ghi chú</span>
+                                      <textarea name="note" id="note" cols="15" rows="5"></textarea>
+                                  </div>
+
+                                  <div class="thanh_toan">
+                                    <p class="h3">Phương thức thanh toán</p>
+                                    <div class="shp">
+                                        <input type="radio" value="thanhtoan" name="thanhtoan" id="thanhtoan">
+                                        <label for="thanhtoan">Thanh toán khi nhận hàng</label>
+                                    </div>
+                                  </div>
+
+                                  <div class="submit">
+                                    <button type="submit">Thanh toán</button>
+                                  </div>
+                              </div>
+                            </form>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-12 order-1 right-shipping">
+                        <div class="column main">
+                            <div class="block-title h4">Tóm tắt đơn hàng</div>
+
+                            <div class="block-content shipping-content">
+                                <div class="total-price">
+                                    <span>Tổng tiền</span>
+                                    <p class="price">98.200 đ</p>
+                                </div>
+
+                                <div class="total-ship">
+                                    <span>Phí giao hàng</span>
+                                    <p class="price-ship">25.000 đ</p>
+                                </div>
+
+                                <div class="total-grand">
+                                    <span>Phí giao hàng</span>
+                                    <p class="amount">123.200 đ</p>
+                                </div>
+                            </div>
+
+                            <a href="#sp_cart" class="btn btn-info" data-toggle="collapse">
+                                <span class="h4">2 Sản phẩm trong giỏ </span>
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </a>
+                            <div id="sp_cart" class="collapse">
+                                <div class="product">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <img src="https://www.thtruemart.vn/media/catalog/product/cache/033f609347571ebd72d8644cf9057fec/l/o/loc-sua-tuoi-tiet-trung-th-true-milk-light-meal.jpg" width="78" height="78" alt="Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp" title="Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp">
+                                        </div>
+                                        <div class="col-xs-5">
+                                            <div class="product-item-name-block">
+                                                <p class="product-item-name"">Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp</p>
+                                                <div class="details-qty">
+                                                    <span>Số lượng</span>
+                                                    <span class="value">2</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-3">
+                                            <span class="cart-price">
+                                                98.200 đ
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <img src="https://www.thtruemart.vn/media/catalog/product/cache/033f609347571ebd72d8644cf9057fec/u/n/untitled-1_copy.jpg" width="78" height="78" alt="Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp" title="Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp">
+                                        </div>
+                                        <div class="col-xs-5">
+                                            <div class="product-item-name-block">
+                                                <p class="product-item-name"">Lốc sữa tươi tiệt trùng bổ sung ngũ cốc dạng hạt TH true MILK LIGHT MEAL 180ml x 4 hộp</p>
+                                                <div class="details-qty">
+                                                    <span>Số lượng</span>
+                                                    <span class="value">2</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-3">
+                                            <span class="cart-price">
+                                                98.200 đ
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!--  -->
 
         <div class="footer">
@@ -228,16 +381,33 @@
             });
         }
 
-        
+                // Lắng nghe sự kiện nhập liệu vào trường input
+        const nameInput = document.getElementById('name');
+        const phoneInput = document.getElementById('tel');
+        const addrInput = document.getElementById('addr');
+
+
+nameInput.addEventListener('input', function(event) {
+  // Cập nhật nội dung của div dựa trên giá trị đã nhập
+  $('.name').html("Họ và tên: " + event.target.value);
+});
+
+phoneInput.addEventListener('input', function(event) {
+  // Cập nhật nội dung của div dựa trên giá trị đã nhập
+  $('.phone').html("Số điện thoại: " + event.target.value);
+});
+
+addrInput.addEventListener('input', function(event) {
+  // Cập nhật nội dung của div dựa trên giá trị đã nhập
+  $('.addr').html("Địa chỉ: " + event.target.value);
+});
+
     </script>
 
     <script src="../js/app.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"
-        integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
