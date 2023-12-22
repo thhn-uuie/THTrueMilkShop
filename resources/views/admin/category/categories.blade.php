a0ec68dab7c29e9206933a8c9cd7feb194b824e9
thhn-uuie <117459480+thhn-uuie@users.noreply.github.com>
2023-12-22 15:31:34 +0700

    Update categories.blade.php

--- resources/views/admin/category/categories.blade.php
+++ resources/views/admin/category/categories.blade.php
@@ -150,13 +150,7 @@
                             </table>
                         </div>
                         <div class="card-footer clearfix">
-                            <ul class="pagination pagination m-0 float-right">
-                                <li class="page-item"><a class="page-link" href="#">«</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">»</a></li>
-                            </ul>
+                            {{ $categories->links() }}
                         </div>
                     </div>
                 </div>
